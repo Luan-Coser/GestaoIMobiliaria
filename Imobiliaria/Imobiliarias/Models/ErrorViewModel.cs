e8226362d4d3718919511993294cec8a7344b97e
Luan-Coser <Luan.coser@gmail.com>
2024-09-10 19:58:30 -0300

    Resolusão atividade 14

---
+++ Imobiliaria/Imobiliarias/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Imobiliarias.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
